e7f074c7718d734f6b9e11f43c124b289d8b0a7a
Ekene Izukanne <ekeneizukanne@gmail.com>
2021-10-10 22:54:39 +0100

    refactor and fix bug

--- Weather.js
+++ Weather.js
@@ -4,75 +4,76 @@ import Forecast from "./Forecast";
 import FetchForecast from './FetchForcast';
 
 export default class Weather extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { zip: "", forecast: null};
-    }
+  constructor(props) {
+    super(props);
+    this.state = { zip: "", forecast: null};
+  }
 
-    handleInput = event => {
-        let zip = event.nativeEvent.text;
-        this.setState({zip})
-        FetchForecast.fetchForecast(zip).then(forecast => {
-            this.setState({ forecast: forecast });
-        });
-    }
+  handleInput = event => {
+    let zip = event.nativeEvent.text;
+    this.setState({zip})
+    FetchForecast.fetchForecast(zip).then(forecast => {
+      this.setState({ forecast: forecast });
+    });
+  }
 
-    render() {
-        let content = null;
-        if (this.state.forecast !== null) {
+  render() {
+    let content = null;
+    const baseFontSize = 16;
+    const styles = StyleSheet.create({
+      container: { flex: 1, alignItems: "center", paddingTop: 30 },
+      backdrop: { flex: 1, flexDirection: "column" },
+      overlay: {
+        paddingTop: 5,
+        backgroundColor: "#000000",
+        opacity: 0.5,
+        flexDirection: "column",
+        alignItems: "center"
+      },
+      row: {
+        flexDirection: "row",
+        flexWrap: "nowrap",
+        alignItems: "flex-start",
+        padding: 60
+      },
+      zipContainer: {
+        height: baseFontSize + 10,
+        borderBottomColor: "#DDDDDD",
+        borderBottomWidth: 1,
+        marginLeft: 5,
+        marginTop: 3
+      },
+      zipCode: { flex: 1, flexBasis: 1, width: 50, height: baseFontSize },
+      mainText: { fontSize: baseFontSize, color: "#FFFFFF" }
+    });
+
+    if (this.state.forecast !== null) {
         content = (
             <Forecast
-            main={this.state.forecast.main}
-            description={this.state.forecast.description}
-            temp={this.state.forecast.temp}
+              main={this.state.forecast.main}
+              description={this.state.forecast.description}
+              temp={this.state.forecast.temp}
             />
         )
-        return (
-            <View style={styles.container}>
-              <ImageBackground source={require('./AppBg.jpg')} resizeMode='cover' style={styles.backdrop}>
-                <View style={styles.overlay}>
-                  <View style={styles.row}>
-                    <Text style={styles.mainText}>
-                        Current weather for
-                    </Text>
-                    <View style={styles.zipContainer}>
-                      <TextInput style={[styles.zipCode, styles.mainText]} onSubmitEditing={this.handleInput} 
-                                  underlineColorAndroid="transparent"/>
-                    </View>
-                  </View>
-                  {content}
-                </View>
-              </ImageBackground>
-            </View>
-          );
     }
+
+    return (
+      <View style={styles.container}>
+        <ImageBackground source={require('./AppBg.jpg')} resizeMode='cover' style={styles.backdrop}>
+          <View style={styles.overlay}>
+            <View style={styles.row}>
+              <Text style={styles.mainText}>
+                Current weather for
+              </Text>
+              <View style={styles.zipContainer}>
+                <TextInput style={[styles.zipCode, styles.mainText]} onSubmitEditing={this.handleInput} 
+                  underlineColorAndroid="transparent"/>
+              </View>
+            </View>
+            {content}
+          </View>
+        </ImageBackground>
+      </View>
+    );
+  }
 }
-
-const baseFontSize = 16;
-
-const styles = StyleSheet.create({
-  container: { flex: 1, alignItems: "center", paddingTop: 30 },
-  backdrop: { flex: 1, flexDirection: "column" },
-  overlay: {
-    paddingTop: 5,
-    backgroundColor: "#000000",
-    opacity: 0.5,
-    flexDirection: "column",
-    alignItems: "center"
-  },
-  row: {
-    flexDirection: "row",
-    flexWrap: "nowrap",
-    alignItems: "flex-start",
-    padding: 60
-  },
-  zipContainer: {
-    height: baseFontSize + 10,
-    borderBottomColor: "#DDDDDD",
-    borderBottomWidth: 1,
-    marginLeft: 5,
-    marginTop: 3
-  },
-  zipCode: { flex: 1, flexBasis: 1, width: 50, height: baseFontSize },
-  mainText: { fontSize: baseFontSize, color: "#FFFFFF" }
-});
